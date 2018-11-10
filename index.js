1275f675b1e31cccc7c1369aa035f07a00db8aa2
AdhirajNakhe <nakhe93@ufl.edu>
2018-11-10 11:38:28 -0500

    Indentation

--- index.js
+++ index.js
@@ -3,37 +3,37 @@ var sqlite3 = require('sqlite3').verbose();
 var constants = require('./constants.js');
 
 let instantiateDb = () => {
-    return new sqlite3.Database(':memory:');
+  return new sqlite3.Database(':memory:');
 }
 
-let createTable = async(db, tableName, schema) => {
-    let query = createTableCommand() + tableName + openParanthesis();
-    
-    query += addColumns(schema);
-    query += closeParanthesis();
+let createTable = async (db, tableName, schema) => {
+  let query = createTableCommand() + tableName + openParanthesis();
 
-    await db.run(query);
+  query += addColumns(schema);
+  query += closeParanthesis();
+
+  await db.run(query);
+}
+
+let addColumns = schema => {
+  let query = "";
+
+  for (key in schema) {
+    query += key + " ";
+    query += schema[key];
+    query += query + ", ";
   }
 
-  let addColumns = schema => {
-      let query = "";
+  if (query.length !== 0)
+    query = removeLastComma(query);
 
-      for(key in schema){
-        query += key + " ";
-        query += schema[key];
-        query += query + ", ";
-      }
+  return query;
+}
 
-      if(query.length !== 0)
-        query = removeLastComma(query);
+let removeLastComma = query => query.substring(0, query.length - 2);
 
-      return query;
-  }
+let createTableCommand = () => constants.CREATE_TABLE;
 
-  let removeLastComma = query => query.substring(0, query.length - 2);
+let openParanthesis = () => constants.OPEN_PARANTHESIS;
 
-  let createTableCommand = () => constants.CREATE_TABLE;
-
-  let openParanthesis = () => constants.OPEN_PARANTHESIS;
-
-  let closeParanthesis = () => constants.CLOSE_PARATHESIS;
+let closeParanthesis = () => constants.CLOSE_PARATHESIS;
